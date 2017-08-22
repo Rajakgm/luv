30e2e072668454b1ba619cbd5a7b122b3d9f5628
Rajakgm <30368233+Rajakgm@users.noreply.github.com>
2017-08-22 16:25:16 +0530

    Create naveee.c

---
+++ naveee.c
@@ -0,0 +1,27 @@
+# luv
+
+#include <stdio.h>
+ 
+void main()
+{
+    int n1, n2, n3;
+ 
+    printf("Enter the values of n1, n2 and n3\n");
+    scanf("%d %d %d", &n1, &n2, &n3);
+    printf("n1 = %d\tn2 = %d\tn3 = %d\n", n1, n2, n3);
+    if (n1 > n2)
+    {
+        if (n1 > n3)
+        {
+            printf("num1 is the greatest among three \n");
+        }
+        else
+        {
+            printf("num3 is the greatest among three \n");
+        }
+    }
+    else if (n2 > n3)
+        printf("num2 is the greatest among three \n");
+    else
+        printf("num3 is the greatest among three \n");
+}
